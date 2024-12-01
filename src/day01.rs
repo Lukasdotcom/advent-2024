2f1790e4482c7a0dfe8c1a2543d10edb49e51034
Lukas <lukas@lschaefer.xyz>
2024-12-01 00:29:10 -0500

    Day 1

--- src/day01.rs
+++ src/day01.rs
@@ -1,75 +1,48 @@
+use std::collections::{BinaryHeap, HashMap};
 // Used to get the data
 const DATA: &str = include_str!("../data/final/day01.txt");
-pub fn part1(data: &str) -> u32 {
-    let data = data.split('\n');
-    let mut total = 0;
-    for i in data {
-        let mut first = None;
-        let mut last = None;
-        for j in i.chars() {
-            let j = j.to_digit(10);
-            if let Some(j) = j {
-                if first.is_none() {
-                    first = Some(j);
-                }
-                last = Some(j);
-            }
-        }
-        total += first.unwrap_or(0) * 10 + last.unwrap_or(0);
-    }
-    total
+#[test]
+fn test_part1() {
+    const DATA_PART1: &str = include_str!("../data/test/day01.txt");
+    assert_eq!(calc(DATA_PART1).0, 11);
 }
-const NUMBERS: [&str; 9] = [
-    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-];
-pub fn part2(data: &str) -> usize {
-    let data = data.split('\n');
-    let mut total = 0;
-    for i in data {
-        let mut first = None;
-        let mut last = None;
-        for j in 0..i.len() {
-            let val = i.chars().nth(j).unwrap_or('a').to_digit(10);
-            if val.is_some() {
-                if first.is_none() {
-                    first = Some(j);
-                }
-                last = Some(j);
-            }
+#[test]
+fn test_part2() {
+    const DATA_PART2: &str = include_str!("../data/test/day01.txt");
+    assert_eq!(calc(DATA_PART2).1, 31);
+}
+pub fn calc(data: &str) -> (u32, u32) {
+    let mut first_half = BinaryHeap::new();
+    let mut second_half = BinaryHeap::new();
+    let mut second_half_amount = HashMap::<u32, u32>::default();
+    for line in data.lines() {
+        let mut nums = line
+            .split(' ')
+            .map(|s| s.parse::<u32>())
+            .filter(|n| n.is_ok())
+            .map(|n| n.unwrap());
+        let next = nums.next();
+        if next.is_some() {
+            first_half.push(next.unwrap());
         }
-        let mut num1 = i
-            .chars()
-            .nth(first.unwrap_or(0))
-            .unwrap()
-            .to_digit(10)
-            .unwrap_or(0) as usize;
-        let mut num2 = i
-            .chars()
-            .nth(last.unwrap_or(0))
-            .unwrap()
-            .to_digit(10)
-            .unwrap_or(0) as usize;
-        for (num, string) in NUMBERS.iter().enumerate() {
-            let loc: Option<usize> = i
-                .get(0..first.unwrap_or(string.len()))
-                .unwrap_or("")
-                .find(string);
-            if let Some(loc) = loc {
-                num1 = num + 1;
-                first = Some(loc + string.len());
-            }
-            while let Some(loc) = i.get(last.unwrap_or(0)..).unwrap_or("").find(string) {
-                num2 = num + 1;
-                last = Some(last.unwrap_or(0) + loc + 1);
-            }
+        let next = nums.next();
+        if next.is_some() {
+            second_half.push(next.unwrap());
+            *second_half_amount.entry(next.unwrap()).or_default() += 1;
         }
-        total += num1 * 10 + num2;
     }
-    total
+    let mut sum_part1 = 0;
+    let mut sum_part2 = 0;
+    while first_half.len() > 0 || second_half.len() > 0 {
+        let first = first_half.pop().unwrap();
+        let second = second_half.pop().unwrap();
+        sum_part1 += first.abs_diff(second);
+        sum_part2 += second_half_amount.get(&first).unwrap_or(&0) * first;
+    }
+    (sum_part1, sum_part2)
 }
 pub fn main() {
-    let answer = part1(DATA);
-    println!("Answer for day 1 part 1 is {}.", answer);
-    let answer = part2(DATA);
-    println!("Answer for day 1 part 2 is {}.", answer);
+    let (part1, part2) = calc(DATA);
+    println!("Answer for day 1 part 1 is {}.", part1);
+    println!("Answer for day 1 part 2 is {}.", part2);
 }
